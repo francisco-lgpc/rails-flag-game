c7c54334198dcdb4faeb3c446d2ba5d2d72a49be
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-09-04 14:40:10 +0200

    wip leaderboard

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,5 +1,10 @@
 class LeaderboardController < ApplicationController
   def index
     @games = Game.where(mode: params[:game_mode]).order(:score).reverse.first(10)
+    p @games
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 end
