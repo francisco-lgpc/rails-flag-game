3fe9c4f4ca366f51b2705f39be2dcddf3a5fa5d0
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-09-06 20:33:30 +0100

    added leaderboard link without log in

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,4 +1,5 @@
 class LeaderboardController < ApplicationController
+	skip_before_action :authenticate_user!
   def index
   	game_mode = params[:game_mode] || 1
     @games = Game.where(mode: game_mode).order(:score).reverse.first(10)
