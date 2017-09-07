1ce8c8361db78c50a440428cd9ad141f6f746077
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-09-07 22:55:05 +0100

    small change in leaderboard controller

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,9 +1,8 @@
 class LeaderboardController < ApplicationController
-	skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
   def index
   	game_mode = params[:game_mode] || 1
     @games = Game.where(mode: game_mode).order(:score).reverse.first(10)
-    p @games
     respond_to do |format|
       format.js
       format.html
